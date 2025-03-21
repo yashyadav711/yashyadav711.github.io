26e5fa6415cb3517c0b4edd33e301597a3211780
yashyadav711 <yashyadav711@yahoo.in>
2025-03-21 05:45:20 +0530

    Added more Tools

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
 function navigateTo(tool) {
-    const toolPaths = {
-        calculator: "tools/calculator.html",
-        converter: "tools/currency-converter.html",
-        notes: "tools/text-editor.html",
-        todo: "tools/color-palette-generator.html"
-    };
-
-    if (toolPaths[tool]) {
-        window.location.href = toolPaths[tool];
-    } else {
-        console.error("Tool not found!");
-    }
+    window.location.href = `tools/${tool}.html`;
 }
